12725c3211f3c59945b0e144e4ceec4b1b0b5b70
Dion Kanhai <dionkanhai@Dions-MacBook-Air.local>
2023-10-04 20:32:00 -0400

    First commit

---
+++ src/components/SportsOdds.jsx
@@ -0,0 +1,7 @@
+export default function SportsOdds() {
+  return (
+    <>
+      <h1>Welcome to the Sports Odd Page!</h1>
+    </>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import SportsOdds from './components/SportsOdds';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SportsOdds/>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
