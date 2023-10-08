909da927b84f98231343cebf8124fb5f8711d7e2
Dion Kanhai <dionkanhai@dions-air.home>
2023-10-08 14:16:02 -0400

    test api

--- src/components/SportsOdds.jsx
+++ src/components/SportsOdds.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+
 export default function SportsOdds() {
+  useEffect(() => {
+    fetch(
+      `https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=418db928118d8482455764850c4098cb`
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error fetching data", error));
+  }, []);
+
   return (
     <>
       <h1>Welcome to the Sports Odd Page!</h1>
