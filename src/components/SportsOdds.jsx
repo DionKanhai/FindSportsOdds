8873f31af5b56d41deaa807c82592f89c4e97803
Dion Kanhai <dionkanhai@Dions-MacBook-Air.local>
2023-10-09 20:09:07 -0400

    call api and fetch data

--- src/components/SportsOdds.jsx
+++ src/components/SportsOdds.jsx
@@ -1,18 +1,44 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
 
 export default function SportsOdds() {
+
+  // state to store the fetched from the API, null because no data is fetched initally
+const [sportsData, setSportsData] = useState(null);
+
+const [error, setError] = useState(null);
+
+const apiKey = '418db928118d8482455764850c4098cb';
+
+
   useEffect(() => {
     fetch(
-      `https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=418db928118d8482455764850c4098cb`
+      `https://api.the-odds-api.com/v4/sports/upcoming/odds/?regions=us&markets=h2h&oddsFormat=american&apiKey=${apiKey}`
     )
       .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error("Error fetching data", error));
+      .then((data) => { 
+        console.log("api response", data)
+        setSportsData(data) })
+      .catch((error) => console.error("Error fetching data", error))
   }, []);
 
   return (
-    <>
-      <h1>Welcome to the Sports Odd Page!</h1>
-    </>
+    <div>
+      {error ? (
+        <p>Error fetching data: {error.message}</p>
+      ) : sportsData ? (
+        <div>
+          {sportsData.map((sport) => (
+            <div key={sport.id}>
+              <h2>{sport.sport_title}</h2>
+              <h4>{sport.away_team}</h4>
+              <h4>{sport.home_team}</h4>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p> Loading... </p>
+      )}
+    </div>
   );
 }
